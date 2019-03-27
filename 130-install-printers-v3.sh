8cf36f9b57661f3487f217ff5835250d3b389b2f
technopig10 <36379158+technopig10@users.noreply.github.com>
2019-03-27 09:03:10 -0400

    Update 130-install-printers-v3.sh

--- 130-install-printers-v3.sh
+++ 130-install-printers-v3.sh
@@ -24,7 +24,7 @@ sudo pacman -S --noconfirm --needed cups cups-pdf
 sudo pacman -S ghostscript gsfonts gutenprint --noconfirm --needed
 sudo pacman -S gtk3-print-backends --noconfirm --needed
 sudo pacman -S libcups --noconfirm --needed
-sudo pacman -S hplip --noconfirm --needed
+#sudo pacman -S hplip --noconfirm --needed
 sudo pacman -S system-config-printer --noconfirm --needed
 
 sudo systemctl enable org.cups.cupsd.service
