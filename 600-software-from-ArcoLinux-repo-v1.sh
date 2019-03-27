d92baf0e48f004a67f57e6c01d9fa6b3da01302a
technopig10 <36379158+technopig10@users.noreply.github.com>
2019-03-27 09:29:40 -0400

    Update 600-software-from-ArcoLinux-repo-v1.sh

--- 600-software-from-ArcoLinux-repo-v1.sh
+++ 600-software-from-ArcoLinux-repo-v1.sh
@@ -34,17 +34,17 @@ sudo pacman -S arcolinux-bin-git --noconfirm --needed
 sudo pacman -S arcolinux-config-plasma-git --noconfirm --needed
 #sudo pacman -S arcolinux-config-xmobar-git --noconfirm --needed
 #sudo pacman -S arcolinux-config-xmonad-git --noconfirm --needed
-sudo pacman -S arcolinux-conky-collection-git --noconfirm --needed
+#sudo pacman -S arcolinux-conky-collection-git --noconfirm --needed
 #sudo pacman -S arcolinux-docs-git --noconfirm --needed
 #sudo pacman -S arcolinux-faces-git --noconfirm --needed
 sudo pacman -S arcolinux-fonts-git --noconfirm --needed
-sudo pacman -S arcolinux-geany-git --noconfirm --needed
+#sudo pacman -S arcolinux-geany-git --noconfirm --needed
 #sudo pacman -S arcolinux-i3wm-git --noconfirm --needed
 #sudo pacman -S arcolinux-kvantum-git --noconfirm --needed
 #sudo pacman -S arcolinux-kvantum-plasma-git --noconfirm --needed
 #sudo pacman -S arcolinux-lightdm-gtk-greeter --noconfirm --needed
-sudo pacman -S arcolinux-lightdm-gtk-greeter-plasma --noconfirm --needed
-sudo pacman -S arcolinux-lightdm-gtk-greeter-settings --noconfirm --needed
+#sudo pacman -S arcolinux-lightdm-gtk-greeter-plasma --noconfirm --needed
+#sudo pacman -S arcolinux-lightdm-gtk-greeter-settings --noconfirm --needed
 #sudo pacman -S arcolinux-local-git --noconfirm --needed
 #sudo pacman -S arcolinux-logo-git --noconfirm --needed
 sudo pacman -S arcolinux-mirrorlist-git --noconfirm --needed
@@ -59,7 +59,7 @@ sudo pacman -S arcolinux-neofetch-git --noconfirm --needed
 #sudo pacman -S arcolinux-obmenu-generator-minimal-git --noconfirm --needed
 #sudo pacman -S arcolinux-openbox-configs-git --noconfirm --needed
 #sudo pacman -S arcolinux-openbox-themes-git --noconfirm --needed
-sudo pacman -S arcolinux-pipemenus-git --noconfirm --needed
+#sudo pacman -S arcolinux-pipemenus-git --noconfirm --needed
 sudo pacman -S arcolinux-plank-git --noconfirm --needed
 sudo pacman -S arcolinux-plank-themes-git --noconfirm --needed
 #sudo pacman -S arcolinux-polybar-git --noconfirm --needed
@@ -72,10 +72,10 @@ sudo pacman -S arcolinux-qt5-plasma-git --noconfirm --needed
 #sudo pacman -S arcolinux-slim --noconfirm --needed
 #sudo pacman -S arcolinux-slimlock-themes-git --noconfirm --needed
 sudo pacman -S arcolinux-system-config-git --noconfirm --needed
-sudo pacman -S arcolinux-termite-themes-git --noconfirm --needed
+#sudo pacman -S arcolinux-termite-themes-git --noconfirm --needed
 #sudo pacman -S arcolinux-tint2-git --noconfirm --needed
 #sudo pacman -S arcolinux-tint2-themes-git --noconfirm --needed
-sudo pacman -S arcolinux-variety-git --noconfirm --needed
+#sudo pacman -S arcolinux-variety-git --noconfirm --needed
 sudo pacman -S arcolinux-wallpapers-git --noconfirm --needed
 #sudo pacman -S arcolinux-xfce4-panel-profiles-git --noconfirm --needed
 #sudo pacman -S arcolinux-xfce-thunar-git --noconfirm --needed
@@ -92,17 +92,17 @@ echo "################################################################"
 echo "Copying all files and folders from /etc/skel to ~"
 echo "################################################################"
 echo
-cp -rT /etc/skel ~
+#cp -rT /etc/skel ~
 
 echo "################################################################"
 echo "####     Software from ArcoLinux 3party Repository           ####"
 echo "################################################################"
 echo "Retry if it times out - large files to download"
 
-sudo pacman -S --noconfirm --needed discord
-sudo pacman -S --noconfirm --needed dropbox
+#sudo pacman -S --noconfirm --needed discord
+#sudo pacman -S --noconfirm --needed dropbox
 #sudo pacman -S --noconfirm --needed insync
-sudo pacman -S --noconfirm --needed spotify
+#sudo pacman -S --noconfirm --needed spotify
 
 echo "################################################################"
 echo "####     Software from ArcoLinux 3party Repository installed       ####"
@@ -124,4 +124,4 @@ echo "################################################################"
 echo "removing to fix variety ~/.config/variety"
 echo "################################################################"
 echo
-rm -rf ~/.config/variety
+#rm -rf ~/.config/variety
