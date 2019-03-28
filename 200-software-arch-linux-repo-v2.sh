69c33f122a93a95e74e4bde35a42bcb80b36415d
technopig10 <36379158+technopig10@users.noreply.github.com>
2019-03-28 09:20:15 -0400

    Update 200-software-arch-linux-repo-v2.sh

--- 200-software-arch-linux-repo-v2.sh
+++ 200-software-arch-linux-repo-v2.sh
@@ -132,7 +132,7 @@ sudo pacman -S --noconfirm --needed screenfetch
 #sudo pacman -S --noconfirm --needed scrot
 sudo pacman -S --noconfirm --needed simple-scan
 sudo pacman -S --noconfirm --needed sysstat
-#sudo pacman -S --noconfirm --needed terminator
+sudo pacman -S --noconfirm --needed kdeplasma-addons
 #sudo pacman -S --noconfirm --needed termite
 #sudo pacman -S --noconfirm --needed thunar
 #sudo pacman -S --noconfirm --needed thunar-archive-plugin
